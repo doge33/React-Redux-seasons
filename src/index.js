82f047d1d30749810a024e7774612c3c90a6e4da
Sandy Long <sandylx33@gmail.com>
2021-01-10 19:16:36 -0500

    initial refactor to class components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import SeasonDisplay from './SeasonDisplay';
+
+// const App = () => {
+
+  
+//   return (
+//     <div>Latitude: </div>
+//   );
+// }
+
+//class-based components
+//extends React.Component because you need many methods that comes with it. render() is one of them
+class App extends React.Component {
+
+  //a JS method not specific to React. constructor means the first method to be called any time an instance of this class is created
+  constructor(props) {
+    super(props);  //constructor in App naturally overrides constructor in React.Component; but to make sure what's in the constuctor in React.Component can still be called, we need to super it.
+
+    this.state = {lat: null}; //initialize state
+
+  }
+
+  //render method is required for every class. Must also DEFINE it.
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log("the error is", err)
+    );
+    return <div> Latitude:</div>
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <App />,
+  document.querySelector('#root')
+)
